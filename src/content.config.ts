68211dd554007ae9f35bc426e8e9de6f9ffc9680
Himoryzhang <himory.zhang@gmail.com>
2025-11-24 12:01:23 +0800

    feat: add about collection and update blog post routing

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,15 +1,28 @@
+import { file, glob } from 'astro/loaders';
 import { defineCollection, z } from 'astro:content';
 
 const blog = defineCollection({
-  type: 'content',
+  loader: glob({ pattern: '*.md', base: "src/content/blog/posts", generateId: (file) => {
+    console.log(file);
+    return file.entry.split('/').pop()?.replace('.md', '') ?? '';
+  }}),
   schema: z.object({
     title: z.string(),
     description: z.string(),
     pubDate: z.coerce.date(),
-    heroImage: z.string().optional()
   })
 });
 
+const about = defineCollection({
+  loader: glob({ pattern: 'about.md', base: "src/content/blog" }),
+  schema: z.object({
+    title: z.string(),
+    description: z.string(),
+    pubDate: z.coerce.date(),
+  })
+})
+
 export const collections = {
-  blog
+  blog,
+  about
 };
